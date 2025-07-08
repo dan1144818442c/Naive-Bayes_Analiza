e7fa3627564b34056d9e231d0dd189f5ac12bea2
d.c! <daniel.c99999@gmail.com>
2025-07-08 12:20:32 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import kagglehub
+import pandas as pd
+# Download latest version
+# path = kagglehub.dataset_download("eswarchandt/phishing-website-detector")
+#
+# print("Path to dataset files:", path)
+# print(pd.read_csv('DATA_CSV/CSV_phishing.csv'))
