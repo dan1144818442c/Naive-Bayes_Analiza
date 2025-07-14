7b5bdbceb3c7cec16cc0475ab87b3fe1d1a55dd1
d.c! <daniel.c99999@gmail.com>
2025-07-14 15:29:10 +0300

    update the logger that will be with date

--- logging_/Logging.py
+++ logging_/Logging.py
@@ -3,5 +3,5 @@ import  datetime
 def Log(message):
     path = r"C:\Users\1\Desktop\DATA_Analiza\Naive Bayes\logging_\log.txt"
     with open(path , 'a') as file:
-        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file.write('\n' + message + "     : " + current_time + '\n')
