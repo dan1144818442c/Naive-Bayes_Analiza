c8ebaa8cb1e47c367f23a9ea166d0bd6896f7347
d.c! <daniel.c99999@gmail.com>
2025-07-22 15:47:14 +0300

    update docker - one docker for all project

--- APP/request.py
+++ APP/request.py
@@ -4,12 +4,26 @@ import model.Base_classified as Base_classified
 import json
 import pandas as pd
 from DATA_CSV.clean_data import clean_nall_and_duplicates
-
 import logging_.Logging
 
 app = FastAPI()
 
+df_phishing = pd.read_csv("DATA_CSV/CSV_phishing.csv", index_col='Index')
+df_phishing_clean = clean_nall_and_duplicates(df_phishing)
+phishing_classifier = Base_classified.classified(data_frame=df_phishing_clean, target_column='Label')
+logging_.Logging.Log("CREATE  phishing_classifier ")
+df_computer = pd.read_csv("DATA_CSV/CSV_buy_comuter.csv", index_col='id')
+df_computer_clean = clean_nall_and_duplicates(df_computer)
+computer_classifier = Base_classified.classified(data_frame=df_computer_clean, target_column='Category')
 
+df_titanic = pd.read_csv("DATA_CSV/CSV_titanic.csv")
+df_titanic_clean = clean_nall_and_duplicates(df_titanic)
+titanic_classifier = Base_classified.classified(data_frame=df_titanic_clean, target_column='Survived')
+classifiers = {
+    'df_titanic': titanic_classifier,
+    'df_computer': computer_classifier,
+    'df_phishing': phishing_classifier
+}
 @app.get("/predict/{database_name}")
 async def predict(database_name: str, request: Request):
     if database_name not in classifiers :
@@ -27,22 +41,7 @@ async def predict(database_name: str, request: Request):
     }
 
 
-if __name__ == "__main__":
-    df_phishing = pd.read_csv(r"C:\Users\1\Desktop\DATA_Analiza\Naive Bayes\DATA_CSV\CSV_phishing.csv", index_col='Index')
-    df_phishing_clean = clean_nall_and_duplicates(df_phishing)
-    phishing_classifier = Base_classified.classified(data_frame=df_phishing_clean, target_column='Label')
-
-    df_computer = pd.read_csv(r"C:\Users\1\Desktop\DATA_Analiza\Naive Bayes\DATA_CSV\CSV_buy_comuter.csv", index_col='id')
-    df_computer_clean = clean_nall_and_duplicates(df_computer)
-    computer_classifier = Base_classified.classified(data_frame=df_computer_clean, target_column='Category')
-
-    df_titanic = pd.read_csv(r"C:\Users\1\Desktop\DATA_Analiza\Naive Bayes\DATA_CSV\CSV_titanic.csv")
-    df_titanic_clean = clean_nall_and_duplicates(df_titanic)
-    titanic_classifier = Base_classified.classified(data_frame=df_titanic_clean, target_column='Survived')
-    classifiers = {
-        'df_titanic': titanic_classifier,
-        'df_computer': computer_classifier,
-        'df_phishing': phishing_classifier
-    }
-
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#
+#
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
